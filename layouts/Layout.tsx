9f6765ac86cd1cf41c6d2b1c97bd379effa2b450
Zeithrold <zeithrold@gmail.com>
2022-11-13 11:36:07 +0800

    fixed build error

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,5 +1,4 @@
 import BetaHeaderBar from '@/components/BetaHeaderBar';
-import BirthdayHeaderBar from '@/components/BirthDayHeaderBar';
 // import CloudCommonHeader from '@/components/CloudHeaderBar';
 import React from 'react';
 
@@ -7,7 +6,6 @@ export default function Layout({ children }: { children: any }) {
   return (
     <div>
       {/* <CloudCommonHeader /> */}
-      <BirthdayHeaderBar />
       <BetaHeaderBar />
       {children}
     </div>
